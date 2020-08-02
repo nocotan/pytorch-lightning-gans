00e8c69e2011ff596f8979bc50e01e83934c6fcb
nocotan <noconoco.lib@gmail.com>
2020-08-02 14:34:36 +0900

    update to us gpus

--- models/dcgan.py
+++ models/dcgan.py
@@ -210,7 +210,7 @@ def main(args: Namespace) -> None:
     # ------------------------
     # If use distubuted training  PyTorch recommends to use DistributedDataParallel.
     # See: https://pytorch.org/docs/stable/nn.html#torch.nn.DataParallel
-    trainer = Trainer()
+    trainer = Trainer(gpus=args.gpus)
 
     # ------------------------
     # 3 START TRAINING
@@ -220,6 +220,7 @@ def main(args: Namespace) -> None:
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+    parser.add_argument("--gpus", type=int, default=0, help="number of GPUs")
     parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
     parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
     parser.add_argument("--b1", type=float, default=0.5,
